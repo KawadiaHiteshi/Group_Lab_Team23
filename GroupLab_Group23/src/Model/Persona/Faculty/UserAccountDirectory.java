02c69d86fc65abd1333b52ec8b414c0eeb38baef
prekshapraveen1407 <praveen.p@northeastern.edu>
2025-10-19 05:00:20 -0400

    Model.Persona.Faculty package

---
+++ GroupLab_Group23/src/Model/Persona/Faculty/UserAccountDirectory.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model.Persona.Faculty;
+
+import Model.Persona.Person;
+import Model.Persona.UserAccount;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author prekshapraveen
+ */
+public class UserAccountDirectory {
+    Department department;
+    ArrayList<UserAccount> studentlist;
+
+    public UserAccountDirectory(Department d) {
+
+        department = d;
+        studentlist = new ArrayList();
+
+    }
+
+    public UserAccount newUserAccount(Person p) {
+
+        UserAccount sp = new UserAccount(p);
+        studentlist.add(sp);
+        return sp;
+    }
+
+    public UserAccount findStudent(String id) {
+
+        for (UserAccount sp : studentlist) {
+
+         //   if (sp.isMatch(id)) {
+         //       return sp;
+         //   }
+        }
+            return null; //not found after going through the whole list
+         }
+}
