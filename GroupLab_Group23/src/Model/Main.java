40c02fd7908002f786fc1e2ce0fa87c4306e3206
jayahahahaha <147718802+jaya44444@users.noreply.github.com>
2025-10-18 23:56:45 -0400

    created main

---
+++ GroupLab_Group23/src/Model/Main.java
@@ -0,0 +1,9 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main;
+import model.Member1;
+import model.Member2;
+import model.Member3;
+import model.Member4;
