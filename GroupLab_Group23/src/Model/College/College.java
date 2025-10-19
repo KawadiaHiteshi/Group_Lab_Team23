4129cededfc9e122dea0df353a39922136c6d807
prekshapraveen1407 <praveen.p@northeastern.edu>
2025-10-19 02:45:21 -0400

    Model.College package

---
+++ GroupLab_Group23/src/Model/College/College.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model.College;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author prekshapraveen
+ */
+public class College {
+    ArrayList<Department> departments;
+    
+    public College(String name){
+        departments = new ArrayList();
+    }
+}
