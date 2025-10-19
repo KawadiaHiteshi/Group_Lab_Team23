3af826baa87fee91ed1a90a08e115c967705100a
jayahahahaha <147718802+jaya44444@users.noreply.github.com>
2025-10-19 01:57:18 -0400

    updated main

--- GroupLab_Group23/src/Member2.java
+++ GroupLab_Group23/src/Member2.java
@@ -2,12 +2,11 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Model;
-public class Main {
-    public static void main(String[] args) {
-     Member1.main(args);
-     member2.main(args);
-     Member3.main(args);
-     Member4.main(args);
-}
+
+/**
+ *
+ * @author Jayashree
+ */
+class Member2 {
+    
 }
